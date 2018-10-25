8e8e6acb6a16421587ea4d6a4bd7f951c1bdc081
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-10-25 08:04:44 -0700

    a

--- src/cache.c
+++ src/cache.c
@@ -22,13 +22,6 @@ struct cache_entry *alloc_entry(char *path, char *content_type, void *content, i
  */
 void cache_free(struct cache *cache)
 {
-    struct cache_entry *cur_entry = cache->head;
-     hashtable_destroy(cache->index);
-     while (cur_entry != NULL) {
-        struct cache_entry *next_entry = cur_entry->next;
-         free_entry(cur_entry);
-         cur_entry = next_entry;
-    }
 }
 
 /**
